46b3daa6eb9c9cf451a86caa3881339453b1a75f
stiles69 <brett.salemink@gmail.com>
2018-06-24 16:25:02 -0500

    Update

---
+++ funcGenerateGPGKey.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: funcGenerateGPGKey.sh
+# 
+#         USAGE: ./funcGenerateGPGKey.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), brett.salemink@gmail.com
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 06/24/2018 15:51
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+funcGenerateGPGKey ()
+{ 	
+ssh-keygen -t dsa
+gpg --gen-key
+
+}
