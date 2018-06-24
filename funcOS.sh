40d350821f8567150742a2d1fc8cc985fe3574df
stiles69 <brett.salemink@gmail.com>
2018-06-24 06:17:56 -0500

    Update

--- funcOS.sh
+++ funcOS.sh
@@ -1,4 +1,4 @@
-#!/bin/bash - 
+#!/bin/bash  
 #===============================================================================
 #
 #          FILE: funcOS.sh
@@ -20,33 +20,30 @@
 set -o nounset                              # Treat unset variables as an error
 
 
-OS ()
+funcOS ()
 {
+local OS
+
 if [ -f /etc/os-release ]; then
     	# freedesktop.org and systemd    
 	. /etc/os-release    
 	OS=$NAME    
-	VER=$VERSION_ID
 elif type lsb_release >/dev/null 2>&1; then    
 	# linuxbase.org    
 	OS=$(lsb_release -si)    
-	VER=$(lsb_release -sr)
 elif [ -f /etc/lsb-release ]; then    
 	# For some versions of Debian/Ubuntu without lsb_release command    
 	. /etc/lsb-release    
 	OS=$DISTRIB_ID    
-	VER=$DISTRIB_RELEASE
 elif [ -f /etc/debian_version ]; then    
 	# Older Debian/Ubuntu/etc.    
-	OS=Debian    
-	VER=$(cat /etc/debian_version)
+	OS='Debian'    
 elif [ -f /etc/SuSe-release ]; then    
-	# Older SuSE/etc.    
-	...elif [ -f /etc/redhat-release ]; then    
-	# Older Red Hat, CentOS, etc.    
-	...else    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.    
-	OS=$(uname -s)    
-	VER=$(uname -r)
+	# Older SuSE/etc.   
+        OS='SuSe'	
+else    OS='Other'
+
 fi
 
-}	#--End OS
+echo $OS
+}
