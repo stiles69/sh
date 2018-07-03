c914b272e4ac041059c32cd8e8323276d32ea971
stiles69 <brett.salemink@gmail.com>
2018-07-03 09:17:33 +0100

    Update

---
+++ funcTimer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: funcTimer.sh
+# 
+#         USAGE: ./funcTimer.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 03/07/18 06:08
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+function Timer () 
+{
+	COUNTDOWN=$1
+	LONGCOMMAND=$2
+
+	( sleep $COUNTDOWN ) & $LONGCOMMAND
+	seconds=1
+	while jobs %1 &>/dev/null ; do    
+		echo $((seconds++)) "seconds complete"
+		sleep 1    
+	done
+	
+	echo "Done."
+	
+}	# end function
+
