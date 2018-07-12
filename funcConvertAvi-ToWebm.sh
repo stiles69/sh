5262ea37fd001eb9d6f997ed7732803f236d6d86
stiles69 <brett.salemink@gmail.com>
2018-07-12 17:26:40 -0500

    manjaro Bash

---
+++ funcConvertAvi-ToWebm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: funcConvertAvi-ToWebm.sh
+# 
+#         USAGE: ./funcConvertAvi-ToWebm.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/12/2018 17:12
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+function ConvertAviToWebm ()
+{
+
+	MYVIDEO="$1"
+	ffmpeg -i "$MYVIDEO" -f webm -c:v libvpx -b:v 1M -acodec libvorbis "$MYVIDEO.webm" -hide-banner
+
+}	# end function
+
+
