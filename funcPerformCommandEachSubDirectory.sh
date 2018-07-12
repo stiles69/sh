68e471a3b12808ceb0a73204cc1d587ab05a251b
stiles69 <brett.salemink@gmail.com>
2018-07-12 11:56:15 -0500

    manjaro Bash

---
+++ funcPerformCommandEachSubDirectory.sh
@@ -0,0 +1,39 @@
+#!/bin/bash  
+#===============================================================================
+#
+#          FILE: PerfomCommandEachSubDirectory.sh
+# 
+#         USAGE: ./PerfomCommandEachSubDirectory.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 07/12/2018 11:24
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+function PerformCommandEachSubDirectory ()
+{
+	if [ "$#" -lt 1 ]
+	then
+		echo "You need at least one arguement which is the command."
+		exit 0
+	else
+		CUSTOMCOMMAND="$1"
+		for d in [0-9][0-9][0-9]
+		do
+			( cd $D && $CUSTOMCOMMAND )
+		done
+	fi
+}	# end function
+
+# == Exit ==
+exit 0
