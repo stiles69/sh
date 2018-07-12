ecf66e14e715e7d6a712b459f725d1f760ad6c47
stiles69 <brett.salemink@gmail.com>
2018-07-12 03:26:23 -0500

    Update from Bash

---
+++ funcWalk.sh
@@ -0,0 +1,49 @@
+#!/bin/bash  
+#===============================================================================
+#
+#          FILE: Walk-Thru-Subdirectories-Example.sh
+# 
+#         USAGE: ./Walk-Thru-Subdirectories-Example.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), brett.salemink@gmail.com
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 06/29/2018 01:47
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+shopt -s globstar
+
+
+function Walk ()
+{
+for i in ./**/*
+do
+	if [ -f "$i" ];
+	then        
+		#printf "Path: %s\n" "${i%/*}" # shortest suffix removal
+		#printf "Filename: %s\n" "${i##*/}" # longest prefix removal
+		#printf "Extension: %s\n"  "${i##*.}"
+		#printf "\n\n"
+		#printf "Filesize: %s\n" "$(du -b "$i" | awk '{print $1}')"
+	       	FILEEXT="${i##*.}"	
+		FILENAME="${i##*/}"
+		#NAME="$(echo "$FILENAME" | cut -d'.' -f1)"
+
+		#echo "FILEXT is "$FILEEXT
+		#echo "FILENAME is $FILENAME"
+		#echo "NAME =  $NAME"
+		COMMAND="$1"
+		echo "$COMMAND $FILENAME"
+		fi
+		
+done
+}	# End function
+	
