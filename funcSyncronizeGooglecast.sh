9a7711e6afffa2e617d6b074c13a3cfdc934ffa4
stiles69 <brett.salemink@gmail.com>
2018-07-12 16:17:17 -0500

    manjaro Bash

--- funcSyncronizeGooglecast.sh
+++ funcSyncronizeGooglecast.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 #===============================================================================
 #
-#          FILE: Ffmpeg-mp4-To-Googlecast.sh
+#          FILE: Syncronize-Googlecast.sh
 # 
-#         USAGE: ./Ffmpeg-mp4-To-Googlecast.sh 
+#         USAGE: ./Syncronize-Googlecast.sh 
 # 
 #   DESCRIPTION: 
 # 
@@ -13,26 +13,18 @@
 #         NOTES: ---
 #        AUTHOR: Brett Salemink (), admin@roguedesigns.us
 #  ORGANIZATION: Rogue Designs
-#       CREATED: 07/12/2018 15:10
+#       CREATED: 07/12/2018 16:05
 #      REVISION:  ---
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 
-function Demux ()
+function SyncronizeGooglecast ()
 {
-	# Must be MP4 Format
-	PARAM1="$1"
-	MYVIDEO=`echo $PARAM1 | cut -d'.' -f1`
-	FILEEXT="${PARAM1##*.}"	
-	if [ "$FILEEXT" = "mp4" ]
-	then
+	MYVIDEO="$1"
 
-		ffmpeg -i "$MYVIDEO.mp4" -vcodec copy -vn "myvideo_video.mp4"
-		ffmpeg -i "$MYVIDEO.mp4" -acodec copy -vn "myvideo_audio.m4a"
-	else
-		echo "Wrong extension must be mp4"
-		exit 0
-	fi
+	ffmpeg -i "$MYVIDEO" -s -keyint_min 150 -g 150 -f webm -vf setsar=1:1 "$MYVIDEO.webm"
+
 
 }	# end function
+
