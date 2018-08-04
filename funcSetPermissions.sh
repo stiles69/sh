43f274e39a56cbf450d9f7cb3a6f4c115b66bc9b
Brett Salemink <brett.salemink@gmail.com>
2018-08-03 22:24:33 -0500

    manjaro Bash

---
+++ funcSetPermissions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE: funcSetPermissions.sh
+# 
+#         USAGE: ./funcSetPermissions.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 08/03/2018 22:11
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+USAGE="The correct usage of this function is funcSetPermissions.sh needs to be sourced and to call this function SetPermissions [The Directory to set permissions on.]"
+
+function SetPermissions ()
+{
+	if [ "$1" ]
+	then
+		local SETPERMISSIONSDIR="$1"
+		sudo chown -R brettsalemink:users "$SETPERMISSIONSDIR"
+		sudo chmod -R 774 "$SETPERMISSIONSDIR"
+	else
+		echo "$USAGE"
+		echo "Exiting..."
+		exit 1
+	fi
+
+	echo "Permissions changed to Owner brettsalemink Group users and 774 for $SETPERMISSIONSDIR"
+}
+
