4800bd5491caa00a4b474591d38f9c4d4d71305f
stiles69 <brett.salemink@gmail.com>
2018-08-11 17:40:39 -0500

    Update

---
+++ funcReadTextFileIntoArray.sh
@@ -0,0 +1,35 @@
+#!/bin/bash  
+#=========================================================
+#
+#          FILE: funcReadTextFileIntoArray.sh
+# 
+#         USAGE: ./funcReadTextFileIntoArray.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (BS), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 08/11/2018 17:24
+#      REVISION:  ---
+#=========================================================
+set -o nounset                              # Treat unset variables as an error
+
+function ReadTextFileIntoArray ()
+{
+	USAGE="This will take a text file that and read each line into an array and assign it a number. The output is ${a[$counter]}. Where a is the line of text followed by a number."
+
+	counter=0
+	readarray a < testinput.txt
+	cat testinput.txt | while read myline; 
+	do
+		echo value is: ${a[$counter]}
+    		counter=$(($counter+1))
+	done
+	return "${a[@]}"
+
+}
+
