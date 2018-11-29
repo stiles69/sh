072e1ef6471412437ced0c8534bc308081e29b72
stiles69 <brett.salemink@gmail.com>
2018-11-29 01:54:24 -0600

    manjaro Bash

---
+++ funcReplaceUnderlineWithSpace.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+#===============================================================================
+#
+#          FILE: funcDate.sh
+# 
+#         USAGE: ./funcDate.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Salemink (), admin@roguedesigns.us
+#  ORGANIZATION: Rogue Designs
+#       CREATED: 08/04/2018 02:34
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+function Date ()
+{
+	THEVARIABLE = "$1" | sed -e 's/_/ /g)
+	echo "$THEVARIABLE"
+}	# end function
